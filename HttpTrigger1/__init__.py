019b9407b39121e1d20fca9185fa8ac8830288af
ThibaultDieudonne <43320476+ThibaultDieudonne@users.noreply.github.com>
2022-03-16 14:51:25 +0100

    first commit, working demo

---
+++ HttpTrigger1/__init__.py
@@ -0,0 +1,40 @@
+import logging
+
+import azure.functions as func
+import pandas as pd
+import pickle
+
+INVALID_REQUEST_MESSSAGE = "This HTTP triggered function needs a valid user_id (provided as /HttpTrigger1?user_id=X)"
+
+def get_predictions(user_id, n=5):
+    user_articles = set(clicks_df[clicks_df["user_id"]==user_id]["click_article_id"].to_list())
+    scores = [(article, model.predict(user_id, article).est) for article in left_articles if article not in user_articles]
+    scores.sort(key=lambda x: x[1], reverse=True)
+    return [x[0] for x in scores[:n]]
+
+clicks_df = pd.read_csv('./clicks_df.csv')
+left_articles = set(clicks_df['click_article_id'].to_list())
+with open(r"baseline.pickle", "rb") as input_file:
+    model = pickle.load(input_file)
+
+def main(req: func.HttpRequest) -> func.HttpResponse:
+    logging.info('Python HTTP trigger function processed a request.')
+
+    user_id = req.params.get('user_id')
+    if not user_id:
+        return func.HttpResponse(
+             INVALID_REQUEST_MESSSAGE,
+             status_code=200
+        )
+
+    else:
+        try:
+            user_id = int(user_id)
+        except:
+            return func.HttpResponse(
+             INVALID_REQUEST_MESSSAGE,
+             status_code=400
+        )
+        return func.HttpResponse(
+            f"Predictions: {get_predictions(user_id)}",
+            status_code=200)
